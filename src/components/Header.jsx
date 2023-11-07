358a591e6e1874c269eed2dace76f0cd1f9bdda9
Kevin LeRoy <17477183+klleroy@users.noreply.github.com>
2023-11-07 10:11:58 -0700

    Adding react-router-bootstrap for navigation.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { FaRegEnvelope, FaUserFriends, FaUsersCog } from 'react-icons/fa';
 import logo from '/images/logo50x50.png';
 
@@ -7,23 +8,33 @@ const Header = () => {
 		<header>
 			<Navbar bg='dark' data-bs-theme='dark' expand='lg' collapseOnSelect>
 				<Container>
-					<img src={logo} alt='CrashLabs' />
-					<Navbar.Brand href='/'>CrashLabs</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand>
+							<img src={logo} alt='CrashLabs' />
+							CrashLabs
+						</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
-							<Nav.Link href='/about'>
-								About &nbsp;
-								<FaUserFriends />
-							</Nav.Link>
-							<Nav.Link href='/services'>
-								Services &nbsp;
-								<FaUsersCog />
-							</Nav.Link>
-							<Nav.Link href='/contact'>
-								Contact &nbsp;
-								<FaRegEnvelope />
-							</Nav.Link>
+							<LinkContainer to='/about'>
+								<Nav.Link>
+									About &nbsp;
+									<FaUserFriends />
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/services'>
+								<Nav.Link>
+									Services &nbsp;
+									<FaUsersCog />
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/contact'>
+								<Nav.Link>
+									Contact &nbsp;
+									<FaRegEnvelope />
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
