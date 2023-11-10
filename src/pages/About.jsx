63d7ffed55c9a86ad30e4caec481ca5247aaa257
Kevin LeRoy <17477183+klleroy@users.noreply.github.com>
2023-11-10 14:18:26 -0700

    Updated grammar.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ const bios = [
 
 	'I have over fifteen years of technical experience in Web Development, Cloud Computing and Server Management. I aim to bring effective practical solutions which will positively impact your business.',
 
-	'On a personal level, I have several passions that keep me busy outside of work. First and foremost, I have 2 daughters who are active in their Sporting Activities. Our oldest, is just ramping up her Volleyball career after being involved in competitive Dance for a few years. Our youngest, is involved in American Ninja Warrior which keeps her and us quite busy!',
+	'On a personal level, I have several passions that keep me busy outside of work. First and foremost, I have 2 daughters who are active in their sporting activities. Our oldest, is just ramping up her Volleyball career after being involved in competitive Dance for a few years. While our youngest, is involved in American Ninja Warrior which keeps her and us quite busy!',
 
 	"When I'm not hanging with the family, I enjoy watching Sports. I especially love watching Baseball and College Basketball. Having lived in the Seattle area for a number of years, it is incredibly exciting to watch Playoff Baseball. Go Dbacks!!",
 
